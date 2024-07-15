4c10064966c6d9e776d3b447e64ee073db24d126
HuC6280 <desside.dev@gmail.com>
2024-07-15 07:35:25 +0300

    dependencies adding -> react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}>
+      {/* Nothing */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
